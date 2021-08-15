03569b522d8035ef3dc8623ba752fa83dc235d7a
pj1234678 <cooljellbean@gmail.com>
2021-08-14 19:22:56 -0700

    Update client.py
    Add Support for SSL
    Add Support For Password
    Fix Chunk Size for Latency
    Change paInt to paFloat for more robust sound
    Added a  check to ensure the buffer is full

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import ssl
 import socket
 import threading
 import pyaudio
@@ -7,22 +8,26 @@ import pyaudio
 class Client:
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ssl_sock = ssl.wrap_socket(self.s, ca_certs="server.crt",cert_reqs=ssl.CERT_REQUIRED)
         
-        while 1:
+        while True:
             try:
                 self.target_ip = input('Enter IP address of server --> ')
                 self.target_port = int(input('Enter target port of server --> '))
+                self.password = input('Enter password to server --> ')
 
-                self.s.connect((self.target_ip, self.target_port))
+
+                self.ssl_sock.connect((self.target_ip, self.target_port))
+                self.ssl_sock.sendall(self.password.encode())
 
                 break
             except:
                 print("Couldn't connect to server")
 
         chunk_size = 1024 # 512
-        audio_format = pyaudio.paInt16
-        channels = 1
-        rate = 20000
+        audio_format = pyaudio.paFloat32
+        channels = 2
+        rate = 48000
 
         # initialise microphone recording
         self.p = pyaudio.PyAudio()
@@ -38,18 +43,21 @@ class Client:
     def receive_server_data(self):
         while True:
             try:
-                data = self.s.recv(1024)
+                data = self.ssl_sock.recv(1024)
                 self.playing_stream.write(data)
-            except:
-                pass
+                free = self.playing_stream.get_write_available()
+                if (free > 1024) :
+                	tofill = free
+                	#print(tofill)
+                	self.playing_stream.write(chr(0) * tofill)
+            except Exception as e: print(e)
 
 
     def send_data_to_server(self):
         while True:
             try:
                 data = self.recording_stream.read(1024)
-                self.s.sendall(data)
-            except:
-                pass
+                self.ssl_sock.sendall(data)
+            except Exception as e: print(e)
 
 client = Client()
