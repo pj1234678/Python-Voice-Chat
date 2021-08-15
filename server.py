eaa19d01c99da315cf5862ed3b43479e86753ba2
pj1234678 <cooljellbean@gmail.com>
2021-08-14 19:27:01 -0700

    Update server.py
    Added Password Support
    Added SSL/TLS Support
    Allowed Echo when 1 person is inside the chat room
    Set Buffer size to 1024 to match Client.

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
+import ssl
 import socket
 import threading
 
 class Server:
     def __init__(self):
             self.ip = socket.gethostbyname(socket.gethostname())
-            while 1:
+            while True:
                 try:
                     self.port = int(input('Enter port number to run on --> '))
+                    self.password = input('Enter password to server --> ')
 
                     self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     self.s.bind((self.ip, self.port))
@@ -25,29 +27,43 @@ class Server:
 
         print('Running on IP: '+self.ip)
         print('Running on port: '+str(self.port))
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.load_cert_chain('server.crt', 'server.key')
         
         while True:
             c, addr = self.s.accept()
+            connstream = context.wrap_socket(c, server_side=True)
+            print('New Connection From: ' + addr[0])
 
-            self.connections.append(c)
+            self.connections.append(connstream)
 
-            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
+            threading.Thread(target=self.handle_client,args=(connstream,addr,)).start()
         
     def broadcast(self, sock, data):
         for client in self.connections:
-            if client != self.s and client != sock:
+            if client != self.s and client != sock or len(self.connections) == 1:
                 try:
                     client.send(data)
                 except:
                     pass
 
-    def handle_client(self,c,addr):
-        while 1:
-            try:
-                data = c.recv(1024)
-                self.broadcast(c, data)
-            
-            except socket.error:
-                c.close()
+    def handle_client(self,connstream,addr):
+        connstream.send(str.encode('PASSWORD:'))
+        try:
+            password = connstream.recv(1024)
+            password = password.decode()
+            if password == self.password:
+                while True:
+                    try:
+                        data = connstream.recv(1024)
+                        self.broadcast(connstream, data)
+                    except socket.error:
+                        connstream.close()
+            else:
+                connstream.close()
+                print("Client Dropped Due to Wrong Password")
+        except:
+            connstream.close()
+            print("Client Dropped Due to Unknown Error")
 
 server = Server()
